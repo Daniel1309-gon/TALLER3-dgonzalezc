1cc517bce5599d05a79462f60d91f8dbe78b4fd0
Daniel1309-gon <dgonzalezca@unal.edu.co>
2024-07-12 21:19:20 -0500

    first comit

---
+++ model/boa_constrictor.py
@@ -0,0 +1,26 @@
+from models.animal_exotico import AnimalExotico
+
+class Boa_Constrictor(AnimalExotico):
+    def __init__(self, nombre: str, peso: float, edad: int, pais_origen: str, impuestos: float) -> None:
+        super().__init__(nombre, peso, edad, pais_origen, impuestos)
+        self.__ratones_comidos = 0
+        
+    def hacer_sonido(self) -> None:
+        return '¡Tsss!'
+    
+    def comer_ratones(self, ratones: int) -> None:
+        if self.__ratones_comidos + ratones > 10:
+            raise ValueError('Demasiados ratones!')
+        else:
+            self.__ratones_comidos += ratones
+    
+    def obtener_ratones_comidos(self) -> int:
+        return self.__ratones_comidos
+    
+    def alimentar(self) -> str:
+        try:
+            self.comer_ratones(1)
+            return "Éxito"
+        except Exception as e:
+            return str(e)
+    
---
+++ model/guarderia.py
@@ -0,0 +1,22 @@
+from models.boa_constrictor import Boa_Constrictor
+from models.huron import Huron
+
+class Guarderia():
+    def __init__(self) -> None:
+        self.boas = [Boa_Constrictor('loki', 24.7, 3, 'colombia', 38300),
+                     Boa_Constrictor('isis', 34.7, 8, 'india', 90300)]
+        self.hurones = [Huron("Huron1", 2.0, 3, "USA", 1000.0),
+                        Huron("Huron2", 2.5, 4, "Canadá", 9020.0)]
+        
+    def alimentar_boa(self, boa: Boa_Constrictor):
+        if not boa:
+            return 'Esta boa no existe'
+        try:
+            resultado = boa.alimentar()
+            return resultado
+        except:
+            return 'La boa está llena'
+        
+        
+    
+    
